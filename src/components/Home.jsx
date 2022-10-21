1db2b91cf14c2e3125b3f342ed8da989b1db442f
hasan <hasanakoz1986@gmail.com>
2022-10-21 14:58:42 +0300

    user card component done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,27 @@ import Card from "./Card";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-const Home = () => {};
+const Home = () => {
+  const url = "https://randomuser.me/api/";
+  const [user, setUser] = useState("");
+
+  const getData = async () => {
+    const { data } = await axios(url);
+    console.log(data.results);
+    setUser(data.results);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  //   console.log(data.results);
+  return (
+    <>
+      <div>
+        <Card user={user} />
+      </div>
+      <button onClick={() => getData()}> Random User</button>
+    </>
+  );
+};
 
 export default Home;
